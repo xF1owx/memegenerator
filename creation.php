6d6408b3fc2a6bd316034b1ac7731fe8439876e9
xF1owx <florian.r@codeur.online>
2018-05-15 13:05:57 +0200

    syncro

--- creation.php
+++ creation.php
@@ -11,12 +11,12 @@
 
 <body>
 
-    <form id="uploadForm" method="post" action="models/request.php" enctype="multipart/form-data">
+    <form id="form" method="post" action="models/request.php" enctype="multipart/form-data">
 
         <p>
 
             <input type="hidden" name="MAX_FILE_SIZE" value="32481010">
-            <input type="file" name="path">    </br> 
+            <input type="file" name="upFile">    </br> 
             
             <input type="submit" name="upload" value="Envoyer">
         </p>
--- models/request.php
+++ models/request.php
@@ -1,69 +1,68 @@
 <?php
 
-
-
-$_FILES['icone']['name'] //Le nom original du fichier, comme sur le disque du visiteur (exemple : mon_icone.png).
-$_FILES['icone']['type'] //Le type du fichier. Par exemple, cela peut être « image/png ».
-$_FILES['icone']['size'] //La taille du fichier en octets.
-$_FILES['icone']['tmp_name'] //L'adresse vers le fichier uploadé dans le répertoire temporaire.
-$_FILES['icone']['error'] //Le code d'erreur, qui permet de savoir si le fichier a bien été uploadé.
-
-var_dump($_FILES['icone']['name']);
-
-
-
-
-
-
-
 require_once('../utils/bdd.php');
+
+$_FILES['upFile']['name']; //Le nom original du fichier, comme sur le disque du visiteur (exemple : mon_icone.png).
+// $_FILES['icone']['type'] //Le type du fichier. Par exemple, cela peut être « image/png ».
+// $_FILES['icone']['size'] //La taille du fichier en octets.
+// $_FILES['icone']['tmp_name'] //L'adresse vers le fichier uploadé dans le répertoire temporaire.
+// $_FILES['icone']['error'] //Le code d'erreur, qui permet de savoir si le fichier a bien été uploadé.
+
+var_dump($_FILES['upFile']['name']);
+
+
+
+
+
+
+
 
 
  // insérer le nom de l'image envoyée par l'utilisateur
  //  $sql = "INSERT INTO image (name_image) VALUES (\'recup_img_user\')";
- function uploadImg(){
- 	global $bdd;
- 	$response=$bdd->prepare("INSERT INTO image (name_image) VALUES (\'recup_img_user\')");
- 	$response->execute();
+//  function uploadImg(){
+//  	global $bdd;
+//  	$response=$bdd->prepare("INSERT INTO image (name_image) VALUES (\'recup_img_user\')");
+//  	$response->execute();
 
- 	$result=$response->fetchAll(PDO::FETCH_ASSOC);
+//  	$result=$response->fetchAll(PDO::FETCH_ASSOC);
 
- 	return $result;
-}
+//  	return $result;
+// }
 
-uploadImg();
+// uploadImg();
 
-// affichage des 15 dernières images
-// $sql = "SELECT id_image FROM `image` ORDER BY `id_image` DESC LIMIT 15";
-function showMini(){
-	global $bdd;
-	$response=$bdd->prepare("SELECT id_image FROM `image` ORDER BY `id_image` DESC LIMIT 15");
-	$response->execute();
+// // affichage des 15 dernières images
+// // $sql = "SELECT id_image FROM `image` ORDER BY `id_image` DESC LIMIT 15";
+// function showMini(){
+// 	global $bdd;
+// 	$response=$bdd->prepare("SELECT id_image FROM `image` ORDER BY `id_image` DESC LIMIT 15");
+// 	$response->execute();
 
-	$result=$response->fetchAll(PDO::FETCH_ASSOC);
+// 	$result=$response->fetchAll(PDO::FETCH_ASSOC);
 	
-	var_dump($result);
+// 	var_dump($result);
 
-	return $result;
-}
+// 	return $result;
+// }
 
 
 
-//affiche image séléctionnée dans les miniatures
-// $sql = "SELECT name_image FROM `image` WHERE name_image=\"rangutan-original.jpg\"";
-function showImg($id_image){
-	global $bdd;
-	$response=$bdd->prepare("SELECT name_image FROM `image` WHERE id_image=:idImage");
-	$response->bindParam(":idImage", $id_image, PDO::PARAM_INT);
+// //affiche image séléctionnée dans les miniatures
+// // $sql = "SELECT name_image FROM `image` WHERE name_image=\"rangutan-original.jpg\"";
+// function showImg($id_image){
+// 	global $bdd;
+// 	$response=$bdd->prepare("SELECT name_image FROM `image` WHERE id_image=:idImage");
+// 	$response->bindParam(":idImage", $id_image, PDO::PARAM_INT);
 
-	$response->execute();
+// 	$response->execute();
 
-	$result=$response->fetchAll(PDO::FETCH_ASSOC);
+// 	$result=$response->fetchAll(PDO::FETCH_ASSOC);
 
-	var_dump($result);
+// 	var_dump($result);
 	
-	return $result;
-}
-showImg(); 
+// 	return $result;
+// }
+// showImg(); 
 
 
