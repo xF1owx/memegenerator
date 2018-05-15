0154a89bb09db107328d3ff5ebde735c951a1915
youpiyoful <yoanfornari@gmail.com>
2018-05-15 13:58:45 +0200

    ajout des requètes sql dans models et du dossier utils avec le fichier bdd qui contient new pdo

---
+++ models/request.php
@@ -0,0 +1,54 @@
+<?php
+
+
+require_once('../utils/bdd.php');
+
+
+ // insérer le nom de l'image envoyée par l'utilisateur
+ //  $sql = "INSERT INTO image (name_image) VALUES (\'recup_img_user\')";
+ function uploadImg(){
+ 	global $bdd;
+ 	$response=$bdd->prepare("INSERT INTO image (name_image) VALUES (\'recup_img_user\')");
+ 	$response->execute();
+
+ 	$result=$response->fetchAll(PDO::FETCH_ASSOC);
+
+ 	return $result;
+}
+
+uploadImg();
+
+// affichage des 15 dernières images
+// $sql = "SELECT id_image FROM `image` ORDER BY `id_image` DESC LIMIT 15";
+function showMini(){
+	global $bdd;
+	$response=$bdd->prepare("SELECT id_image FROM `image` ORDER BY `id_image` DESC LIMIT 15");
+	$response->execute();
+
+	$result=$response->fetchAll(PDO::FETCH_ASSOC);
+	
+	var_dump($result);
+
+	return $result;
+}
+
+
+
+//affiche image séléctionnée dans les miniatures
+// $sql = "SELECT name_image FROM `image` WHERE name_image=\"rangutan-original.jpg\"";
+function showImg($id_image){
+	global $bdd;
+	$response=$bdd->prepare("SELECT name_image FROM `image` WHERE id_image=:idImage");
+	$response->bindParam(":idImage", $id_image, PDO::PARAM_INT);
+
+	$response->execute();
+
+	$result=$response->fetchAll(PDO::FETCH_ASSOC);
+
+	var_dump($result);
+	
+	return $result;
+}
+showImg(); 
+
+
---
+++ utils/bdd.php
@@ -0,0 +1,10 @@
+<?php
+try{
+  
+    $bdd=new PDO('mysql:host=localhost;dbname=meme_gen;charset=utf8','root','online@2017');
+    }
+    catch(Exeption $e)
+    {      
+    die('Erreur:'.$e->getMessage());
+    }
+    
