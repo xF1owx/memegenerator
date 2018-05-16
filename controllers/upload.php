a78ec11e70fceeb07d954b760c8804d4fddc7494
xF1owx <florian.r@codeur.online>
2018-05-16 14:39:48 +0200

    suppression doublons

--- controllers/upload.php
+++ controllers/upload.php
@@ -1,24 +1,31 @@
 <?php 
 
 require_once('models/request.php');
-require_once('views/upload.html');
-// var_dump($_FILES);
-// $random = time();
-$path = $_SERVER['DOCUMENT_ROOT'].'/memegenerator/assets/medias/img/'.time().$_FILES['upFile']['name'];
-$resultat = move_uploaded_file($_FILES['upFile']['tmp_name'], $path);
+if (!isset($_FILES['upFile'])){
 
-if ($resultat){
-
-    uploadImg(time().$_FILES['upFile']['name']);
-    header('Location: /memegenerator/creation');
+    require_once('views/upload.html');
 }
-else
-{
-    echo "transfert non réalisé";
-    exit();
+    
+
+
+if (isset($_FILES['upFile'])){
+  
+    $path = $_SERVER['DOCUMENT_ROOT'].'/memegenerator/assets/medias/img/'.time().$_FILES['upFile']['name'];
+    $resultat = move_uploaded_file($_FILES['upFile']['tmp_name'], $path);
+
+    if ($resultat){
+
+        uploadImg(time().$_FILES['upFile']['name']);
+        
+    }
+    else
+    {
+        echo "transfert non réalisé";
+        exit();
+    }
 }
 
-$showMini = showMini();
+// $showMini = showMini();
 
 
 
