28cdfbdd97bcc8ccfad9591de31cbf36e137c4d7
youpiyoful <yoanfornari@gmail.com>
2018-05-22 16:29:16 +0200

    création de render.php render.html, modification du routeur, ajout d'un formulaire d'envoi en post dans creation.html etc

--- controllers/creation.php
+++ controllers/creation.php
@@ -20,10 +20,4 @@ if (isset($_GET['id'])){
 
 }
 
-header ("Content-type: image/jpeg");
-
-$image = imagecreatefromjpeg("'/memegenerator/assets/medias/img/".$creaMeme[0]["name_image"]."'");
-$miniature = imagescale($image, 350);
-imagejpeg($miniature);
-imagedestroy($image);
 
