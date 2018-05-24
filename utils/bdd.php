44207b703dddf57dcdbb0aec2372bd5dc70ce3a0
youpiyoful <yoanfornari@gmail.com>
2018-05-24 16:56:21 +0200

    gestion conflit

--- utils/bdd.php
+++ utils/bdd.php
@@ -2,7 +2,7 @@
 
 try{
   
-    $bdd=new PDO('mysql:host=localhost;dbname=meme_gen;charset=utf8','root','');
+    $bdd=new PDO('mysql:host=localhost;dbname=meme_gen;charset=utf8','root','online@2017');
 }
 catch(Exeption $e)
 {      
